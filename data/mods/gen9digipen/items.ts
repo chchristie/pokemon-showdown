9bb4276ff9e1102f5007a8ea0c4c52263ee7f6c2
chchristie <chchristie@crimson.ua.edu>
2026-05-18 02:37:18 -0500

    fixed typo

--- data/mods/gen9digipen/items.ts
+++ data/mods/gen9digipen/items.ts
@@ -40,7 +40,7 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 		gen: 9,
 		isNonstandard: "DigiPen Future",
 		shortDesc: "If held by a Hydreigon, this item allows it to Mega Evolve in battle.",
-		contributer: ["Bryce G."],
+		contributors: ["Bryce G."],
 	},
 
 	// ── Changing Existing Items ─────────────────────────────────────────────
